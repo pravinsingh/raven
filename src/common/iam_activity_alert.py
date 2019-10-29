478aded50460e4b3f223ca47a7916703bff38631
pravinsingh <this.is.pravin@gmail.com>
2019-10-29 15:25:58 -0700

    Added iam_activity_alert

---
+++ src/common/iam_activity_alert.py
@@ -0,0 +1,28 @@
+import raven
+import os
+
+def handler(event, context):
+    fromEmail = os.environ['fromEmail']
+    toEmails = os.environ['toEmails']
+    subject = event["detail"]["eventName"]
+    event_summary = {
+        "IAM Activity": {
+            "Action": event["detail"]["eventName"],
+            "Action Input": event["detail"]["requestParameters"],
+            "Action Output": event["detail"]["responseElements"],
+            "Region": event["region"],
+            "Created time": event["time"],
+            "Done by": event["detail"]["userIdentity"]["arn"].split(':')[5]
+        }
+    }
+    scroll = raven.Scroll(raven.Severity.Alert)
+    return scroll.send_email(
+        email_from = fromEmail,
+        emails_to = [toEmails],
+        subject = subject,
+        message_type = raven.MessageType.Json,
+        message = event_summary
+    )
+
+if (__name__ == "__main__"):
+    handler(None, None)
