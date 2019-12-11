4ff7841111ea1c20a7af71ab6c45d89abade3ae0
Ruby Zhao <hzhaotibco.com@Rubys-MBP.na.tibco.com>
2019-12-10 16:00:34 -0800

    commit on 'master'

---
+++ src/infra/broker_storage_backup.py
@@ -0,0 +1,133 @@
+""" Base module for project Raven that contains the implementations of Scroll and other 
+    related classes.
+"""
+
+__version__ = '0.8.1'
+__author__ = 'Bhupender Kumar, Ruby Zhao'
+
+import os
+import boto3
+import datetime
+import raven
+
+def handler(event, context):
+    import raven
+    fromEmail = 'tibco-mdm-noreply@tibco.com' #os.environ['fromEmail']
+    toEmail = 'hzhao@tibco.com' #os.environ['toEmail']
+    subject = 'Broker and storage backup' #os.environ['subject']  
+    message = """    
+                                   <br><b style="font-size:18px">Storage Common Dir and Local postgrebackup update</b><br>
+                                        <table >
+                                            <col style="width:25%" span="2" />
+                                        <tr style="background-color: lightgray"><th>Bucket Name</th><th>Job Update</th></tr>"""
+
+    ssm_obj = boto3.client('ssm', region_name='us-east-1')
+    resp_broker = ssm_obj.get_parameter(Name='broker-backup-buckets')
+    broker_buckets = resp_broker['Parameter']['Value'].split(",")
+    resp_storage = ssm_obj.get_parameter(Name='storage-backup-buckets')
+    storage_buckets = resp_storage['Parameter']['Value'].split(",")
+    resp_logs = ssm_obj.get_parameter(Name='logs-backup-buckets')
+    logs_buckets = resp_logs['Parameter']['Value'].split(",")
+    
+    message_1 = "Bucket Name: "
+    message_2 = " file was not uploaded."
+    message_4 = "All files were uploaded successfully"   
+    message_b = "Broker Console DB backup update"
+    message_l = "Most recent Log backups"
+    alert = False
+    
+    s3_client = boto3.client('s3')
+    storage_files = ['dynservices.zip', 'commondir.zip', 'config.zip', 'dbexportfile.zip', 'backuptool.properties.zip']
+    broker_files = ['mdmconsole.zip', 'dbexportfile.zip', 'brokerbackuptool.properties.zip']
+    comp_date = datetime.datetime.now() - datetime.timedelta(days=1)
+    
+    temp_list = []
+    mid_list = []
+    not_present = []          
+    for s1 in storage_buckets:
+        resp = s3_client.list_objects(Bucket=s1)
+        for s2 in resp['Contents']:
+            if s2['LastModified'].replace(tzinfo=None) > comp_date.replace(tzinfo=None):
+                temp_list.append(s2['Key'])
+        for s3 in storage_files:
+            
+            if any(s3 in s for s in temp_list):
+                continue
+            else:
+                mid_list.append(s3)
+
+        if len(mid_list) == 0:
+            message += "<tr><td>%s</td><td style=\"color:green\">%s</td></tr>" %(s1, message_4)
+        else:
+            for s4 in mid_list:
+                temp_msg += s4 + message_2 + "<br>"
+                alert = True
+            message += "<tr><td>%s</td><td style=\"color:red\">%s</td></tr>" %(s1, temp_msg)
+        temp_list = []
+        mid_list = []
+        temp_msg = ""
+
+    message +=  """</table><br>""" 
+    message += """<br><b style="font-size:18px">Broker Console DB backup update</b><br>"""
+    message +=""" <table>
+                    <col style="width:25%" span="2" />
+                    <tr style="background-color: lightgray;"><th>Bucket Name</th><th>Job Update</th></tr>"""
+    for b1 in broker_buckets:
+        resp = s3_client.list_objects(Bucket=b1)
+        for b2 in resp['Contents']:
+            if b2['LastModified'].replace(tzinfo=None) > comp_date.replace(tzinfo=None):
+                temp_list.append(b2['Key'])
+        for b3 in broker_files:
+            if any(b3 in s for s in temp_list):
+                continue
+            else:
+                mid_list.append(b3)
+        if len(mid_list) == 0:
+            message += "<tr><td>%s</td><td style=\"color:green\">%s</td></tr>" %(b1, message_4 )
+        else:
+            for b4 in mid_list:
+                temp_msg += b4 + message_2 + "<br>"
+                alert = True
+            message += "<tr><td>%s</td><td style=\"color:red\">%s</td></tr>" %(b1, temp_msg)
+                
+        temp_list = []
+        mid_list = []
+        temp_msg = ""
+    message +=  """</table><br>"""
+    message += """<br><b style="font-size:20px">Most recent Log backups</b><br>"""
+    message +=""" <table>
+                        <col style="width:25%" span="2" />
+                        <tr style="background-color: lightgray;"><th>Bucket Name</th><th>Logs</th></tr>"""
+
+    dict1 = {}
+    for l1 in logs_buckets:
+        resp = s3_client.list_objects(Bucket=l1)
+        
+        for l2 in resp['Contents']:
+            if l2['LastModified'].replace(tzinfo=None) > comp_date.replace(tzinfo=None):
+                key1 = l2['Key']
+                value1 = l2['LastModified'].strftime('%m/%d/%Y %H:%M:%S')
+                dict1[key1] = value1
+        temp_msg = ""
+        for k, v in dict1.items():
+            temp_msg += k + " " + v + "<br>"
+        message += "<tr><td>%s</td><td>%s</td></tr>" %(l1, temp_msg)
+        
+    message +=  """</table>
+                                """          
+    
+    if alert:
+        scroll = raven.Scroll(raven.Severity.Alert)
+    else:
+        scroll = raven.Scroll(raven.Severity.Report)
+    return scroll.send_email(
+            email_from = fromEmail,
+            emails_to = [toEmail],
+            subject = subject, 
+            message_type = raven.MessageType.Html,
+            message = message         
+    )
+
+
+if (__name__ == "__main__"):
+    handler(None, None)
---
+++ src/infra/raven_broker_storage_snapshot.py
@@ -0,0 +1,62 @@
+import os
+import boto3
+import datetime
+import sys
+sys.path.append("/Users/hzhaotibco.com/codecommit/raven/src")
+from raven.raven import *
+
+
+def handler(event, context):
+    fromEmail = 'tibco-mdm-noreply@tibco.com' #os.environ['fromEmail']
+    toEmail = 'hzhao@tibco.com' #os.environ['toEmail']
+    subject = 'Broker and Storage EBS daily snapshot update' #os.environ['subject']
+    message = """
+                    <table>
+                        <tr style="background-color: lightgray;">
+                        <th>Instance</th>
+                        <th>Instance ID</th>
+                        <th>Volume ID</th>
+                        <th>Snapshot Created</th>
+                        <th>Snapshot Deleted</th></tr>"""
+    # Variables used in output message
+    ins_id = "InstanceID:"
+    snap_cre = "Snapshot Created:"
+    snap_del = "Snapshot Deleted:"
+    vol_id = "VolumeID:"
+    
+    # Looping through all regions
+    ec2_client = boto3.client('ec2')
+    for region in ec2_client.describe_regions()['Regions']:
+        ec2_1 = boto3.client('ec2', region_name=region['RegionName'])
+        res = ec2_1.describe_instances() # Describing EC2 instances in current region
+        for r1 in res['Reservations']:
+            for r2 in r1['Instances']:
+                for r3 in r2['Tags']:
+                    if r3['Value'].find("MDM") != -1 and (r3['Value'].find("roker") != -1 or r3['Value'].find("torage") != -1): # Filtering EC2 instances having a tag containing MDM and any one of Broker and Storage
+                        for v1 in r2['BlockDeviceMappings']:
+                            # Taking snapshot of Broker and Storage EBS volumes
+                            snap = ec2_1.create_snapshot(Description='Snapshot for EBS Volume of' + ' ' + r3['Value']+ ' ' + 'taken on'+ ' ' + datetime.datetime.now().strftime('%m/%d/%Y %H:%M:%S')  , VolumeId=v1['Ebs']['VolumeId'],DryRun=False)
+                            desc_snap = ec2_1.describe_snapshots(Filters=[{'Name':'volume-id','Values': [v1['Ebs']['VolumeId']]}])
+                            comp_date = datetime.datetime.now() - datetime.timedelta(days=2)
+                            for s1 in desc_snap['Snapshots']:
+                                if s1['StartTime'].replace(tzinfo=None) < comp_date.replace(tzinfo=None):
+                                    del_snap = ec2_1.delete_snapshot(SnapshotId=s1['SnapshotId'], DryRun=False) # Deleting snapshots older than specified date(comp_date)
+                                    # Output message to be displayed and mailed to CloudOps
+                                    message += "<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>" %(r3['Value'], r2['InstanceId'], v1['Ebs']['VolumeId'], snap['SnapshotId'], s1['SnapshotId'])
+                                 
+    message +=  """</table>
+                       
+                    """  
+ 
+    scroll = Scroll(Severity.Report)
+    return scroll.send_email(
+            email_from = fromEmail,
+            emails_to = [toEmail],
+            subject = subject, 
+            message_type = MessageType.Html, 
+            message = message            
+    )
+
+if (__name__ == "__main__"):
+    handler(None, None)
+
